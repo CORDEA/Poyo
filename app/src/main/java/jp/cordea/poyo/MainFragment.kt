4d4770f98474e67276508a79707236ceb9e0d6dd
Yoshihiro Tanaka <contact@cordea.jp>
2020-04-26 11:07:04 +0900

    navigate

--- app/src/main/java/jp/cordea/poyo/MainFragment.kt
+++ app/src/main/java/jp/cordea/poyo/MainFragment.kt
@@ -1,5 +1,25 @@
 package jp.cordea.poyo
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.main_fragment.*
 
-class MainFragment : Fragment(R.layout.main_fragment)
+class MainFragment : Fragment(R.layout.main_fragment) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        poyoButton.setOnClickListener {
+            findNavController().navigate(
+                MainFragmentDirections
+                    .actionMainFragmentToPoyoFragment()
+            )
+        }
+        circleButton.setOnClickListener {
+            findNavController().navigate(
+                MainFragmentDirections
+                    .actionMainFragmentToCircleFragment()
+            )
+        }
+    }
+}
